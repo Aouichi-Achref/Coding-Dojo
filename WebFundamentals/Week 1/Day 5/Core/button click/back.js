1ee28b92969f689ca26a5371a2347c401913281d
Aouichi-Achref <aouichi123achref@gmail.com>
2023-10-30 06:15:51 +0100

    commit

---
+++ WebFundamentals/Week 1/Day 5/Core/button click/back.js
@@ -0,0 +1,21 @@
+
+
+function change(element) {
+    if(element.innerText == "login") {
+        element.innerText  = "logout"
+    } else{
+        element.innerText = "login" 
+    }
+}
+
+function hide(button) {
+    button.remove();
+}
+
+
+
+function like(i) {
+    alert("ninja was liked")
+    
+}
+
